84da144ebccfd452bd7dfdadbcb58c80521a3cd0
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-11-28 20:20:59 -0600

    Started a fresh build for project 2. This is in a new repo. There was issues with the last repo and my machine.

---
+++ simpleCalculator/simpleCalculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  simpleCalculator
+//
+//  Created by Zachery Hernandez on 11/28/12.
+//  Copyright (c) 2012 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
